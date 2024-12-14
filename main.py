90fb4ec64a8a418d9fe67598d62e2bd90df97a58
Musharraf Ibragimov <122869450+themusharraf@users.noreply.github.com>
2024-12-14 20:32:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import CommandStart
 import asyncio
 
-BOT_TOKEN = "7569570451:AAFo3LPGklMyWIc5yT7AhNawJKB6pZqsB9Y" # bot new add gits
+BOT_TOKEN = "7569570451:AAFo3LPGklMyWIc5yT7AhNawJKB6pZqsB9Y" # bot new add
 
 bot = Bot(token=BOT_TOKEN) #tt
 dp = Dispatcher()
