d9e5aca10625a39711f49283f27fc293c55cc051
mrx <meibrohimov@gmail.com>
2024-12-14 19:42:06 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import CommandStart, Command
+import asyncio
+
+BOT_TOKEN = "7569570451:AAFo3LPGklMyWIc5yT7AhNawJKB6pZqsB9Y"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
