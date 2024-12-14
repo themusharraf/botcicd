3d820516d6ff6fe17e818fad90d3f15d1d909749
mrx <meibrohimov@gmail.com>
2024-12-14 19:54:35 +0500

    change main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import CommandStart, Command
+from aiogram.filters.command import CommandStart
 import asyncio
 
 BOT_TOKEN = "7569570451:AAFo3LPGklMyWIc5yT7AhNawJKB6pZqsB9Y"
